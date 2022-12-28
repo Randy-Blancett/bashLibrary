686d51bb72fe1cdc8df2344b77f2f494367d2acf
Randy Blancett <Randy.Blancett@gmail.com>
2022-12-28 18:49:09 -0500

    Ticket #9
    Added ability to Check i f user Exists
    Added ability to ensure a user exists.

--- src/test/resources/example/shellUtilsTest.sh
+++ src/test/resources/example/shellUtilsTest.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 BL_PATH=$( dirname $0 )
 BL_PATH=$( readlink -e $BL_PATH )
-echo $BL_PATH
 LIB_PATH=$( dirname $( dirname $( dirname $BL_PATH ) ) )/main/bash/lib
-echo $LIB_PATH
 
 source $LIB_PATH/shellUtils.sh
+VERBOSE=5
 
 parseCmdLine "$@"
 
 isRoot && echo "Am Root" || echo "Not Root"
+
+ensureUser "darkowl1234"
 
 ensureRoot
