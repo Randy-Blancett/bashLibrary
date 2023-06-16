b6d26ab5f78612bbca54c4897d4c285a08be5fa0
Randy Blancett <Randy.Blancett@gmail.com>
2023-06-15 22:59:23 -0400

    ticket #14
    Added Features to BATS Testing

--- src/test/bash/testRunner.sh
+++ src/test/bash/testRunner.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/env bash
 
 echo Running All Unit Tests
-mkdir "${bats.dir}"
-tar --extract --directory "${bats.dir}" --wildcards --strip-components=1 --file "${project.basedir}/target/${bats.zip.name}" */libexec */lib */bin
+BATS_ASSERT_DIR="${bats.dir}/TestHelper/bats-assert"
+BATS_SUPPORT_DIR="${bats.dir}/TestHelper/bats-support"
+mkdir  -p "${bats.dir}"
+tar --extract --directory "${bats.dir}" --wildcards --strip-components=1 --file "${project.basedir}/target/${bats.core.zip.name}" */libexec */lib */bin
+mkdir -p "$BATS_ASSERT_DIR"
+tar --extract --directory "$BATS_ASSERT_DIR" --wildcards --strip-components=1 --file "${project.basedir}/target/${bats.assert.zip.name}" */src */load.bash
+mkdir -p "$BATS_SUPPORT_DIR"
+tar --extract --directory "$BATS_SUPPORT_DIR" --wildcards --strip-components=1 --file "${project.basedir}/target/${bats.support.zip.name}" */src */load.bash
 export LIB_PATH=${test.libDir}
 EXIT_CODE=0;
 for TEST in ./*.bats
