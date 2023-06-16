2591cac8c3d322fec52d3b4373fba447dc4f6f4e
Randy Blancett <Randy.Blancett@gmail.com>
2023-06-15 21:34:59 -0400

    Ticket #14
    Ensure build fails if there are test errors.

--- src/test/bash/testRunner.sh
+++ src/test/bash/testRunner.sh
@@ -4,10 +4,14 @@ echo Running All Unit Tests
 mkdir "${bats.dir}"
 tar --extract --directory "${bats.dir}" --wildcards --strip-components=1 --file "${project.basedir}/target/${bats.zip.name}" */libexec */lib */bin
 export LIB_PATH=${test.libDir}
+EXIT_CODE=0;
 for TEST in ./*.bats
 do
 	echo "*****************************************"
 	echo "* Running Tests in $TEST"
 	echo "*****************************************"
 	${bats} $TEST
+    [[ "$?" > "0" ]] && EXIT_CODE=1;
+	echo status: $?
 done
+exit $EXIT_CODE
